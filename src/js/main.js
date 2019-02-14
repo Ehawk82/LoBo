9c833b1a660f31bb39821ecfdb1f169af770fc3a
Jason Graziano <mrehawk82@gmail.com>
2019-02-14 11:12:14 -0500

    buttons added

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,50 @@ var myUI = {
 		myUI.loadout();
 	},
 	loadout: () => {
-		console.log("working loadout");
+		var dvContain = createEle("div"),
+		    btnFrame = createEle("div"),
+		    dnaBtn = createEle("div"),
+		    invBtn = createEle("div"),
+		    natureBtn = createEle("div"),
+		    storeBtn = createEle("div"),
+		    mapBtn = createEle("div"),
+		    homeBtn = createEle("div"),
+		    settingsBtn = createEle("div");
+        
+        dnaBtn.innerHTML = "💡";
+		dnaBtn.className = "buttons";
+        
+        invBtn.innerHTML = "📦";
+		invBtn.className = "buttons";
+
+        natureBtn.innerHTML = "🌲";
+		natureBtn.className = "buttons";
+
+        storeBtn.innerHTML = "💲";
+		storeBtn.className = "buttons";
+
+        mapBtn.innerHTML = "📜";
+		mapBtn.className = "buttons";
+
+        homeBtn.innerHTML = "🏠";
+		homeBtn.className = "buttons";
+
+		settingsBtn.innerHTML = "⚙";
+		settingsBtn.className = "buttons";
+
+		btnFrame.className = "btnFrame";
+		btnFrame.append(homeBtn);
+		btnFrame.append(settingsBtn);
+		btnFrame.append(mapBtn);
+		btnFrame.append(storeBtn);
+		btnFrame.append(natureBtn);
+		btnFrame.append(invBtn);
+		btnFrame.append(dnaBtn);
+
+        dvContain.className = "dvContain";
+        dvContain.append(btnFrame);
+
+		loboGame.append(dvContain);
 	}
 };
 window.onload = () => {
