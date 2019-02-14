f7457dc298731773a97717afa89515993d93cd20
Jason Graziano <mrehawk82@gmail.com>
2019-02-14 13:50:06 -0500

    from upstairs machine

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,8 @@ var myUI = {
 	loadout: () => {
 		var dvContain = createEle("div"),
 		    btnFrame = createEle("div"),
+		    fsBtn = createEle("div"),
+		    rewardsBtn = createEle("div"),
 		    dnaBtn = createEle("div"),
 		    invBtn = createEle("div"),
 		    natureBtn = createEle("div"),
@@ -13,6 +15,13 @@ var myUI = {
 		    homeBtn = createEle("div"),
 		    settingsBtn = createEle("div");
         
+        fsBtn.innerHTML = "🔳";
+		fsBtn.className = "buttons";
+		fsBtn.onclick = myUI.toggleScreen();
+
+        rewardsBtn.innerHTML = "🌟";
+		rewardsBtn.className = "buttons";
+
         dnaBtn.innerHTML = "💡";
 		dnaBtn.className = "buttons";
         
@@ -42,11 +51,44 @@ var myUI = {
 		btnFrame.append(natureBtn);
 		btnFrame.append(invBtn);
 		btnFrame.append(dnaBtn);
+		btnFrame.append(rewardsBtn);
+		btnFrame.append(fsBtn);
 
         dvContain.className = "dvContain";
         dvContain.append(btnFrame);
 
 		loboGame.append(dvContain);
+	},
+	toggleScreen: () => {
+		return () => {
+			var isInFullScreen = (document.fullscreenElement && document.fullscreenElement !== null) ||
+        (document.webkitFullscreenElement && document.webkitFullscreenElement !== null) ||
+        (document.mozFullScreenElement && document.mozFullScreenElement !== null) ||
+        (document.msFullscreenElement && document.msFullscreenElement !== null);
+
+    		var docElm = document.documentElement;
+    		if (!isInFullScreen) {
+        		if (docElm.requestFullscreen) {
+            		docElm.requestFullscreen();
+        		} else if (docElm.mozRequestFullScreen) {
+            		docElm.mozRequestFullScreen();
+        		} else if (docElm.webkitRequestFullScreen) {
+            		docElm.webkitRequestFullScreen();
+        		} else if (docElm.msRequestFullscreen) {
+            		docElm.msRequestFullscreen();
+        		}
+    		} else {
+        		if (document.exitFullscreen) {
+            		document.exitFullscreen();
+        		} else if (document.webkitExitFullscreen) {
+            		document.webkitExitFullscreen();
+        		} else if (document.mozCancelFullScreen) {
+            		document.mozCancelFullScreen();
+        		} else if (document.msExitFullscreen) {
+            		document.msExitFullscreen();
+        		}
+    		}
+		}
 	}
 };
 window.onload = () => {
