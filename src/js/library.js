59f9f360596beb4c52460a0f4808c582e3de92ca
Jason Graziano <mrehawk82@gmail.com>
2019-02-17 16:31:06 -0500

    building the data pile

---
+++ src/js/library.js
@@ -0,0 +1,11 @@
+homeSect = ["my HOME stuffs", "is here", "...now"];
+
+mapSect = ["my MAP", "is here", "...now"];
+
+marketSect = ["my MARKET", "is here", "...now"];
+
+natureSect = ["my NATURE", "is here", "...now"];
+
+invSect = ["my INVENTORY", "is here", "...now"];
+
+dnaSect = ["my DNA", "is here", "...now"];
